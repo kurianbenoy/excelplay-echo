759c7cfb61b85c581f0322394dc446ee7fe40045
kurianbenoy <kurian.pro@gmail.com>
2018-04-18 13:48:42 +0530

    Add tests

--- excelplay_echo/core/tests.py
+++ excelplay_echo/core/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from core.models import Echouser,EchoUserSubmission
 
-# Create your tests here.
+class ModelTests(TestCase):
+	def setup(self):
+		Echouser.objects.create(userid=13,points=0,rank=100)
+		Echouser.objects.create(userid=100,points=10,rank=90)
+		Echouser.objects.create(userid=1,points=50,rank=40)
+
